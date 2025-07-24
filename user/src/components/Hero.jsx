cb17e39d9172ba13bfb08837ee06f5bfdc413993
Kaif Zafry <kaifzafry3110@gmail.com>
2025-07-25 02:05:54 +0530

    hero section redesigned

--- user/src/components/Hero.jsx
+++ user/src/components/Hero.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 import { BASE_IMG_URL } from "../utils/Constants";
 import { useDispatch, useSelector } from "react-redux";
 import { setCategories } from '../redux/categorySlice';
-
+import { FaArrowRightLong } from "react-icons/fa6";
+import { motion } from "framer-motion";
 const Hero = () => {
   //const [categories, setCategories] = useState([]);
   const categories = useSelector((state) => state.category.list);
@@ -31,8 +32,42 @@ const Hero = () => {
   return (
     <>
       <div className="flat-spacing-34 line-top">
-        <div className="image mb-5 img-style h-full">
-          <img src="/banner2.png" alt="banner" className="lazyload" />
+        <div className="image mb-5 img-style h-full container-full bg-gray-100">
+          <section className="min-h-[80vh] flex items-center justify-center  px-6 py-12">
+      <div className="max-w-6xl h-[80vh] w-full grid md:grid-cols-2 gap-8 items-center">
+        {/* Text Content */}
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+          className="space-y-6"
+        >
+          <h1 className="text-4xl mb-4 md:text-5xl font-bold text-gray-800">
+            Stay Modest. Stay Stylish.
+          </h1>
+          <p className="text-lg text-gray-600">
+            Discover premium Islamic caps crafted for modern style and comfort.
+          </p>
+          <button className="text-base flex items-center gap-2 px-6 py-3 rounded-2xl">
+            Shop Now <FaArrowRightLong />
+          </button>
+        </motion.div>
+
+        {/* Image or 3D Cap */}
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className="flex justify-center"
+        >
+          <img
+            src="/product-3.png"
+            alt="DeenFit Cap"
+            className="w-1/2 max-w-sm rounded-2xl shadow-xl"
+          />
+        </motion.div>
+      </div>
+    </section>
         </div>
         <div className="container-full">
           <h2 className="text-center my-5 title font-7 uppercase">Featured Categories</h2>
@@ -65,7 +100,7 @@ const Hero = () => {
               return (
                 <div
                 key={category.category_id}
-                  className="col-2"
+                  className="col-4 col-md-2"
                   data-aos="fade-up"
                   data-aos-duration="500"
                 >
